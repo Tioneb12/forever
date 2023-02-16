768a0f66d61afe2ec1ea72c744fbd2b84b69bfec
Ben <benoit.zeller@gmail.com>
2023-02-16 21:21:13 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :posts
+
+  mount Avo::Engine, at: Avo.configuration.root_path
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  #root "articles#index"
+  mount Maglev::Engine => '/maglev'
+  get '(*path)', to: 'maglev/page_preview#index', defaults: { path: 'index' }, constraints: Maglev::PreviewConstraint.new
+end
